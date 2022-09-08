fcc4c15c6d45af12d1b75e4ff6bc404c5a0b6f71
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-07 22:19:13 -0700

    added UserCreateRequest class

---
+++ ServiceLambdaModel/src/main/java/com/kenzie/capstone/service/model/UserCreateRequest.java
@@ -0,0 +1,27 @@
+package com.kenzie.capstone.service.model;
+
+public class UserCreateRequest {
+    private String userId;
+    private String username;
+
+    public UserCreateRequest(String userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
