22c1d4ea1ee02d0c6848cdec267bc95d2bbcbd05
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-07 22:18:43 -0700

    changed class name to UserRecord, attribute names and table name

---
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/model/UserRecord.java
@@ -0,0 +1,49 @@
+package com.kenzie.capstone.service.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "Users")
+public class UserRecord {
+
+    private String userId;
+    private String username;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserRecord that = (UserRecord) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(username, that.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username);
+    }
+}
