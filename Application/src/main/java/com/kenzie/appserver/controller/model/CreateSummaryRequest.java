e2b4d1a16182299cd92a90a98335f050d79e4a51
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-11 18:06:47 -0700

    edited fields of CreateSummaryRequest

--- Application/src/main/java/com/kenzie/appserver/controller/model/CreateSummaryRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/CreateSummaryRequest.java
@@ -7,22 +7,35 @@ import javax.validation.constraints.NotEmpty;
 
 public class CreateSummaryRequest {
     @NotEmpty
-    @JsonProperty("gameId")
-    private String gameId;
+    @JsonProperty("gameName")
+    private String gameName;
+    // since we decided to format the key for the game ID in some concatination, when the incoming request comes in
+    // it should contain the raw data the key will be formatted from in the service class
 
     @NotEmpty
+    @JsonProperty("sessionId")
+    private String sessionId;
+
     @JsonProperty("userId")
     private String userId;
 
     @JsonProperty("results")
     private String results;
 
-    public String getGameId() {
-        return gameId;
+    public String getGameName() {
+        return gameName;
     }
 
-    public void setGameId(String gameId) {
-        this.gameId = gameId;
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
     }
 
     public String getUserId() {
