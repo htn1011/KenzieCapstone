9800decdbeb11f345c9d7e09fed08644a0b0268a
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-11 18:07:00 -0700

    commented out unused methods in ExampleService

--- Application/src/main/java/com/kenzie/appserver/service/ExampleService.java
+++ Application/src/main/java/com/kenzie/appserver/service/ExampleService.java
@@ -13,36 +13,36 @@ public class ExampleService {
     private GameRepository gameRepository;
     private UserServiceClient userServiceClient;
 
-    public ExampleService(GameRepository gameRepository, UserServiceClient userServiceClient) {
-        this.gameRepository = gameRepository;
-        this.userServiceClient = userServiceClient;
-    }
+    // public ExampleService(GameRepository gameRepository, UserServiceClient userServiceClient) {
+    //     this.gameRepository = gameRepository;
+    //     this.userServiceClient = userServiceClient;
+    // }
+    //
+    // public Example findById(String id) {
+    //
+    //     // Example getting data from the lambda
+    //     User dataFromLambda = userServiceClient.verifyUser(id);
+    //
+    //     // Example getting data from the local repository
+    //     Example dataFromDynamo = gameRepository
+    //             .findById(id)
+    //             .map(example -> new Example(example.getId(), example.getDate()))
+    //             .orElse(null);
+    //
+    //     return dataFromDynamo;
+    // }
 
-    public Example findById(String id) {
-
-        // Example getting data from the lambda
-        User dataFromLambda = userServiceClient.getExampleData(id);
-
-        // Example getting data from the local repository
-        Example dataFromDynamo = gameRepository
-                .findById(id)
-                .map(example -> new Example(example.getId(), example.getDate()))
-                .orElse(null);
-
-        return dataFromDynamo;
-    }
-
-    public Example addNewExample(String name) {
-        // Example sending data to the lambda
-        User dataFromLambda = userServiceClient.setExampleData(name);
-
-        // Example sending data to the local repository
-        GameSummaryRecord exampleRecord = new GameSummaryRecord();
-        exampleRecord.setId(dataFromLambda.getUserId());
-        exampleRecord.setName(dataFromLambda.getUsername());
-        gameRepository.save(exampleRecord);
-
-        Example example = new Example(dataFromLambda.getUserId(), name);
-        return example;
-    }
+    // public Example addNewExample(String name) {
+    //     // Example sending data to the lambda
+    //     User dataFromLambda = userServiceClient.addNewUser(name);
+    //
+    //     // Example sending data to the local repository
+    //     GameSummaryRecord exampleRecord = new GameSummaryRecord();
+    //     exampleRecord.setId(dataFromLambda.getUserId());
+    //     exampleRecord.setName(dataFromLambda.getUsername());
+    //     gameRepository.save(exampleRecord);
+    //
+    //     Example example = new Example(dataFromLambda.getUserId(), name);
+    //     return example;
+    // }
 }
