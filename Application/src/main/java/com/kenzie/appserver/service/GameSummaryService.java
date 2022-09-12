7ba4fda6ffa46e12c35dfaeca5fcf144e3a7ba96
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-11 18:07:55 -0700

    added in additional method stubs

--- Application/src/main/java/com/kenzie/appserver/service/GameSummaryService.java
+++ Application/src/main/java/com/kenzie/appserver/service/GameSummaryService.java
@@ -5,11 +5,12 @@ import com.kenzie.appserver.controller.model.SummaryResponse;
 import com.kenzie.appserver.repositories.GameRepository;
 import com.kenzie.appserver.repositories.model.GameSummaryRecord;
 import com.kenzie.appserver.service.conversion.GameSummaryConversion;
-import com.kenzie.appserver.service.model.Example;
 import com.kenzie.capstone.service.client.UserServiceClient;
 import com.kenzie.capstone.service.model.User;
+import com.kenzie.capstone.service.model.UserCreateRequest;
+import com.kenzie.capstone.service.model.UserResponse;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public class GameSummaryService {
@@ -21,8 +22,25 @@ public class GameSummaryService {
         this.userServiceClient = userServiceClient;
     }
 
+    public UserResponse addNewUser(UserCreateRequest userCreateRequest) {
+        // request comes from the controller to add a new user
+        // calls the user client
+        return null;
+    }
+
+    public UserResponse verifyUser(String userId) {
+        // calls the user client to verify if user exists
+        return null;
+    }
+
     public SummaryResponse addSummary(CreateSummaryRequest summaryRequest) {
-        User dataFromLambda = userServiceClient.setExampleData(summaryRequest.getUserId());
+        // steps:
+        // use the userService client to verify the user
+        // if the user exists ? continue : give the user an option to create a new user or post without a user ID
+        // create a new summary record and save it to the repo
+        // format the key according to the helper method
+
+        // User dataFromLambda = userServiceClient.addNewUser(summaryRequest.getUserId());
 
         //if user is null, do we want to throw an exception here or handle it in the userServiceClient?
 
@@ -33,7 +51,7 @@ public class GameSummaryService {
     }
 
     public SummaryResponse getSummary(String gameId, String userId) {
-        User dataFromLambda = userServiceClient.getExampleData(userId);
+        User dataFromLambda = userServiceClient.verifyUser(userId);
 
         // if user is null, how do we handle this? Is the exception thrown in the method for userServiceClient checking for null?
 
@@ -44,6 +62,19 @@ public class GameSummaryService {
         }
 
         return GameSummaryConversion.recordToResponse(record.get());
+    }
+
+    public List<SummaryResponse> getAllSummaries() {
+        return null;
+    }
+
+    public List<SummaryResponse> getUserSummaries(String userId) {
+        return null;
+    }
+
+    private String formatKey(String gameName, String sessionId) {
+
+        return String.format("%s::%s", gameName, sessionId);
     }
 
 }
