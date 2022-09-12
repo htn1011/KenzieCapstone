454a2e6638e86d41bd908825af2522240b08d1c6
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-11 18:09:22 -0700

    changed method names

--- ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/UserServiceClient.java
+++ ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/UserServiceClient.java
@@ -1,8 +1,9 @@
 package com.kenzie.capstone.service.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kenzie.capstone.service.model.User;
-
+import com.kenzie.capstone.service.model.UserCreateRequest;
 
 public class UserServiceClient {
 
@@ -15,7 +16,7 @@ public class UserServiceClient {
         this.mapper = new ObjectMapper();
     }
 
-    public User getExampleData(String userId) {
+    public User verifyUser(String userId) {
         EndpointUtility endpointUtility = new EndpointUtility();
         String response = endpointUtility.getEndpoint(VERIFY_USER.replace("{userId}", userId));
         User user;
@@ -27,9 +28,15 @@ public class UserServiceClient {
         return user;
     }
     // this will need to be changed to accept userId and username or would it be json for userrequestobject?
-    public User setExampleData(String data) {
+    public User addNewUser(UserCreateRequest userCreateRequest) {
+        String request;
+        try {
+            request = mapper.writeValueAsString(userCreateRequest);
+        } catch(JsonProcessingException e) {
+            throw new ApiGatewayException("Unable to serialize request: " + e);
+        }
         EndpointUtility endpointUtility = new EndpointUtility();
-        String response = endpointUtility.postEndpoint(ADD_NEW_USER, data);
+        String response = endpointUtility.postEndpoint(ADD_NEW_USER, request);
         User user;
         try {
             user = mapper.readValue(response, User.class);
