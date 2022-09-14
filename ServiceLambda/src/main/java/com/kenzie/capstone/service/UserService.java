0e052d41a01d80b7ea884579256cac71a2f14f3c
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-14 13:56:22 -0700

    added new functions

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/UserService.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/UserService.java
@@ -1,12 +1,15 @@
 package com.kenzie.capstone.service;
 
+import com.kenzie.capstone.service.model.NoExistingUserException;
 import com.kenzie.capstone.service.model.User;
 import com.kenzie.capstone.service.dao.UserDao;
+import com.kenzie.capstone.service.model.UserAlreadyExistsException;
 import com.kenzie.capstone.service.model.UserCreateRequestLambda;
 import com.kenzie.capstone.service.model.UserRecord;
 
 import javax.inject.Inject;
 
+import java.util.List;
 import java.util.Optional;
 
 public class UserService {
@@ -29,7 +32,40 @@ public class UserService {
     }
 
     public User addUser(UserCreateRequestLambda createRequest) {
+        if (findUser(createRequest.getUserId()).isPresent()) {
+            throw new UserAlreadyExistsException(createRequest.getUserId());
+        }
         UserRecord record = userDao.addNewUser(createRequest.getUserId(), createRequest.getUsername());
         return new User(record.getUserId(), record.getUsername(), record.getFriendsList());
     }
+
+    public List<String> getFriends(String userId) {
+        User existingUser = findUser(userId)
+                .orElseThrow(() -> new NoExistingUserException(userId));
+        return existingUser.getFriendsList();
+    }
+
+    public User addfriend(String userId, String friendId) {
+        // make sure user exists
+        User existingUser = findUser(userId)
+                .orElseThrow(() -> new NoExistingUserException(userId));
+        // make sure friend exists
+        User friend = findUser(friendId)
+                .orElseThrow(() -> new NoExistingUserException(friendId));
+        List<String> friendList = existingUser.getFriendsList();
+        friendList.add(friendId);
+        existingUser.setFriendsList(friendList);
+        return existingUser;
+    }
+
+    public User removeFriend(String userId, String friendId) {
+        // make sure user exists
+        User existingUser = findUser(userId)
+                .orElseThrow(() -> new NoExistingUserException(userId));
+        List<String> friendList = existingUser.getFriendsList();
+        friendList.removeIf(id -> id.equals(friendId));
+        existingUser.setFriendsList(friendList);
+        return existingUser;
+    }
+
 }
--- ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/UserServiceClient.java
+++ ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/UserServiceClient.java
@@ -1,15 +1,22 @@
 package com.kenzie.capstone.service.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kenzie.capstone.service.model.UpdateFriendListRequest;
 import com.kenzie.capstone.service.model.User;
 import com.kenzie.capstone.service.model.UserCreateRequestLambda;
 import com.kenzie.capstone.service.model.UserResponseLambda;
+
+import java.util.List;
 
 public class UserServiceClient {
 
     private static final String FIND_EXISTING_USER = "user/{userId}";
     private static final String ADD_NEW_USER = "user";
+    private static final String REMOVE_FROM_USERS_FRIEND = "user/{userId}/removeFriend";
+    private static final String ADD_TO_USERS_FRIENDS = "user/{userId}/addFriend";
+    private static final String FIND_FRIENDS = "user/{userId}/friends";
 
     private ObjectMapper mapper;
 
@@ -45,6 +52,61 @@ public class UserServiceClient {
             throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
         }
         return new UserResponseLambda(user);
+    }
+
+    public List<String> getFriendList(String userId) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(FIND_FRIENDS.replace("{userId}", userId));
+        List<String> friendList;
+        try {
+            friendList = mapper.readValue(response, new TypeReference<>() {
+            });
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return friendList;
+    }
+
+    public UserResponseLambda addFriend(String userId, String friendId) {
+        UpdateFriendListRequest updateFriendListRequest = new UpdateFriendListRequest(userId, friendId);
+        String request;
+        try {
+            request = mapper.writeValueAsString(updateFriendListRequest);
+        } catch(JsonProcessingException e) {
+            throw new ApiGatewayException("Unable to serialize request: " + e);
+        }
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.putEndpoint(ADD_TO_USERS_FRIENDS.replace("{userId}", userId), request);
+        User user;
+        try {
+            user = mapper.readValue(response, User.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return new UserResponseLambda(user);
+    }
+
+    public UserResponseLambda removeFriend(String userId, String friendId) {
+        UpdateFriendListRequest updateFriendListRequest = new UpdateFriendListRequest(userId, friendId);
+        String request;
+        try {
+            request = mapper.writeValueAsString(updateFriendListRequest);
+        } catch(JsonProcessingException e) {
+            throw new ApiGatewayException("Unable to serialize request: " + e);
+        }
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.putEndpoint(
+                REMOVE_FROM_USERS_FRIEND.replace("{userId}", userId), request);
+        User user;
+        try {
+            user = mapper.readValue(response, User.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return new UserResponseLambda(user);
+        // remove a friend from a users list of friends
+        // reutrn updated friend list
+        // return null;
     }
 
     // in client -> design response for expected and/or unexpected behavior
