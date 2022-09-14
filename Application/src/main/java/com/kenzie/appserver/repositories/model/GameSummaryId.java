541f707e6c2f507b565a980764f33532de6bfc03
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-14 13:42:58 -0700

    added ID class to use composite key in table

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/GameSummaryId.java
@@ -0,0 +1,32 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+
+public class GameSummaryId {
+    private String userId;
+    private String SummarySortKey;
+
+    public GameSummaryId(String userId, String summarySortKey) {
+        this.userId = userId;
+        SummarySortKey = summarySortKey;
+    }
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBRangeKey(attributeName = "summarySortKey")
+    public String getSummarySortKey() {
+        return SummarySortKey;
+    }
+
+    public void setSummarySortKey(String summarySortKey) {
+        SummarySortKey = summarySortKey;
+    }
+}
