2fa32ee40f5da5e8fae8a2010db5ac0c8e0df981
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-14 13:30:34 -0700

    added custom methods

--- Application/src/main/java/com/kenzie/appserver/repositories/GameRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/GameRepository.java
@@ -1,10 +1,25 @@
 package com.kenzie.appserver.repositories;
 
+import com.kenzie.appserver.repositories.model.GameSummaryId;
 import com.kenzie.appserver.repositories.model.GameSummaryRecord;
 
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @EnableScan
-public interface GameRepository extends CrudRepository<GameSummaryRecord, String> {
+public interface GameRepository extends CrudRepository<GameSummaryRecord, GameSummaryId> {
+    // find all by date(GSI)
+    List<GameSummaryRecord> findByDate(String date);
+
+    // find all summaries for user - only primary key
+    List<GameSummaryRecord> findByUserId(String userId);
+
+    // find specific gamesummary for user
+    Optional<GameSummaryRecord> findById(GameSummaryId gameSummaryId);
+
+    // delete using the summaryIdClass
+    void deleteById(GameSummaryId gameSummaryId);
 }
