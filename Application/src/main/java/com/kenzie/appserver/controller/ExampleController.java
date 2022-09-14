8f917acc86b9d2395be43eabef8df3a5549de32a
Marika G <73078558+MarikaGregor@users.noreply.github.com>
2022-09-14 13:29:54 -0700

    commented out

--- Application/src/main/java/com/kenzie/appserver/controller/ExampleController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/ExampleController.java
@@ -22,28 +22,28 @@ public class ExampleController {
         this.exampleService = exampleService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ExampleResponse> get(@PathVariable("id") String id) {
-
-        Example example = exampleService.findById(id);
-        if (example == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        ExampleResponse exampleResponse = new ExampleResponse();
-        exampleResponse.setId(example.getId());
-        exampleResponse.setName(example.getName());
-        return ResponseEntity.ok(exampleResponse);
-    }
-
-    @PostMapping
-    public ResponseEntity<ExampleResponse> addNewExample(@RequestBody ExampleCreateRequest exampleCreateRequest) {
-        Example example = exampleService.addNewExample(exampleCreateRequest.getName());
-
-        ExampleResponse exampleResponse = new ExampleResponse();
-        exampleResponse.setId(example.getId());
-        exampleResponse.setName(example.getName());
-
-        return ResponseEntity.ok(exampleResponse);
-    }
+    // @GetMapping("/{id}")
+    // public ResponseEntity<ExampleResponse> get(@PathVariable("id") String id) {
+    //
+    //     Example example = exampleService.findById(id);
+    //     if (example == null) {
+    //         return ResponseEntity.notFound().build();
+    //     }
+    //
+    //     ExampleResponse exampleResponse = new ExampleResponse();
+    //     exampleResponse.setId(example.getId());
+    //     exampleResponse.setName(example.getName());
+    //     return ResponseEntity.ok(exampleResponse);
+    // }
+    //
+    // @PostMapping
+    // public ResponseEntity<ExampleResponse> addNewExample(@RequestBody ExampleCreateRequest exampleCreateRequest) {
+    //     Example example = exampleService.addNewExample(exampleCreateRequest.getName());
+    //
+    //     ExampleResponse exampleResponse = new ExampleResponse();
+    //     exampleResponse.setId(example.getId());
+    //     exampleResponse.setName(example.getName());
+    //
+    //     return ResponseEntity.ok(exampleResponse);
+    // }
 }
