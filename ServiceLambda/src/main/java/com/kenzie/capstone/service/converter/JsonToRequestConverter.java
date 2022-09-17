a6d521c8d007c037863c88043c196d7dc8b52487
syedTshah22 <sshah1322@gmail.com>
2022-09-17 17:06:18 -0500

    Updated Lambdas

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/converter/JsonToRequestConverter.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/converter/JsonToRequestConverter.java
@@ -2,14 +2,25 @@ package com.kenzie.capstone.service.converter;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.kenzie.capstone.service.model.UpdateFriendListRequest;
 import com.kenzie.capstone.service.model.UserCreateRequestLambda;
 
 public class JsonToRequestConverter {
-    public UserCreateRequestLambda convert(String body) {
+    public UserCreateRequestLambda convertToUserCreateRequest(String body) {
         try {
             GsonBuilder builder = new GsonBuilder();
             Gson gson = builder.create();
             return gson.fromJson(body, UserCreateRequestLambda.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Referral could not be deserialized");
+        }
+    }
+
+    public UpdateFriendListRequest convertToUpdateFriendListRequest(String body) {
+        try {
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            return gson.fromJson(body, UpdateFriendListRequest.class);
         } catch (Exception e) {
             throw new IllegalArgumentException("Referral could not be deserialized");
         }
